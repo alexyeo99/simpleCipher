c14c1eeff778a987777362fa715beff6998c76ec
Alex <alex@memetop>
2019-01-10 19:10:51 +0800

    古いコードを見るは痛い！

--- simplecipher.cpp
+++ simplecipher.cpp
@@ -32,72 +32,61 @@ bool squareInit = false;
 */
 string vigenere::decode(string userInput, string rawUserKey) {
   //see if square was not initialized. this will not work if it isn't.
-  if(squareInit == false){
+  if(!squareInit){
     generateSquare();
   }
   string output = "";
-  //Now, process the key to filter invalid stuff.
+  //Now, process the key to filter invalid chars.
   string userKey = "";
-  for (int i = 0; i < rawUserKey.length(); i++)
-    {
+  for(int i = 0; i < rawUserKey.length(); i++){
       char tempChar = tolower(rawUserKey[i]);
-      if (tempChar >= 97 && tempChar <= 122)
-	{
+      if(tempChar >= 97 && tempChar <= 122){
 	  userKey = userKey + tempChar;
-	}
-    }
+      }
+  }
   //get charcount of key and input
   int userInputCnt = userInput.length();
   int userKeyCnt = userKey.length();
   int keyCounter = 0;
 	
-  for (int i = 0; i < userInputCnt; i++)
-    {
+  for(int i = 0; i < userInputCnt; i++){
       char outputChar = 'a';
       char currChar = userInput[i]; // this is so manupulations can be done.
       // Ex: for uppercase / lowercase conversions.
       //add support for upper charachters
       bool makeUpper = false;
-      if (userInput[i] >= 65 && userInput[i] <= 90)
-	{
+      if(userInput[i] >= 65 && userInput[i] <= 90){
 	  makeUpper = true;
 	  //convert to lowercase for compatibility
 	  currChar = tolower(currChar);
-	}
+      }
 
       //make sure only valid chars from user input are processed
-      if ((userInput[i] >= 97 && userInput[i] <= 122) || makeUpper)
-	{
+      if((userInput[i] >= 97 && userInput[i] <= 122) || makeUpper){
 	  //get letter distance from a on key
 	  int charDist = userKey[keyCounter] - 97;
 	  int tempCode = currChar - 97 - charDist;
 	  //handle tempcode underflow
-	  if (tempCode < 0)
-	    {
+	  if(tempCode < 0){
 	      int compensate = 0 - tempCode;
 	      tempCode = 26 - compensate;
-	    }
+	  }
 	  outputChar = vigSquare[0][tempCode];//utilize ony 1 line.
-	  if (makeUpper)
-	    {
+	  if(makeUpper){
 	      outputChar = toupper(outputChar);
-	    }
+	  }
 	  output = output + outputChar; 
 	  //increment char used on key
-
-	  if (keyCounter + 1 >= userKeyCnt)
-	    {
+	  if(keyCounter + 1 >= userKeyCnt){
 	      keyCounter = 0;
-	    }
-	  else {
+	  }
+	  else{
 	    keyCounter++;
 	  }
-	}
-      else
-	{
+      }
+      else{
 	  output = output + (char)userInput[i];
-	}
-		
+      }		
     }
   return output;
 }
@@ -109,27 +98,22 @@ string vigenere::decode(string userInput, string rawUserKey) {
 */
 void vigenere::generateSquare() {
   int w = 0;
-  while (w < 26)
-    {
-      int x = 0;
-      while (x < 26)
-	{
-	  int tempchar = 97 + x + w;
-	  //handle overflows
-	  if (tempchar > 122)
-	    {
-	      int x = tempchar - 122;
-	      tempchar = x + 96;
-	    }
-	  vigSquare[w][x] = (char)tempchar;
-	  x++;
-	}
-      w++;
-    }
+  while (w < 26){
+    int x = 0;
+    while (x < 26){
+      int tempchar = 97 + x + w;
+      //handle overflows
+      if (tempchar > 122){
+	int x = tempchar - 122;
+	tempchar = x + 96;
+      }
+      vigSquare[w][x] = (char)tempchar;
+      x++;
+     }
+     w++;
+  }
   squareInit = true;
 }
-
-
 
 /*
   VIGENERE CIPHER ENCODER FUNCTION
@@ -137,64 +121,54 @@ void vigenere::generateSquare() {
 string vigenere::encode(string userInput, string rawUserKey){
   string output = "";
   //see if square was not initialized. this will not work if it isn't.
-  if(squareInit == false){
+  if(!squareInit){
     generateSquare();
   }
   //Now, process the key to filter invalid stuff.
   string userKey = "";
-  for (int i = 0; i < rawUserKey.length(); i++)
-    {
+  for(int i = 0; i < rawUserKey.length(); i++){
       char tempChar = tolower(rawUserKey[i]);
-      if (tempChar >= 97 && tempChar <= 122)
-	{
+      if (tempChar >= 97 && tempChar <= 122){
 	  userKey = userKey + tempChar;
-	}
-    }
+      }
+  }
   //get charcount of key and input
   int userInputCnt = userInput.length();
   int userKeyCnt = userKey.length();
   int keyCounter = 0;
 
-  for (int i = 0; i < userInputCnt; i++)
-    {
+  for (int i = 0; i < userInputCnt; i++){
       char outputChar = 'a';
       char currChar = userInput[i]; // this is so manupulations can be done.
       // Ex: for uppercase / lowercase conversions.
       //adds support for upper charachters
       bool makeUpper = false; //deermines if the char is uppercase.
-      if (userInput[i] >= 65 && userInput[i] <= 90)
-	{
+      if(userInput[i] >= 65 && userInput[i] <= 90){
 	  makeUpper = true;
 	  //convert to lowercase for compatibility
 	  currChar = tolower(currChar);
 	}
 
       //make sure only valid chars from user input are processed
-      if ((userInput[i] >= 97 && userInput[i] <= 122) || makeUpper)
-	{
+      if((userInput[i] >= 97 && userInput[i] <= 122) || makeUpper){
 	  //get letter distance from a on key
 	  int charDist = userKey[keyCounter] - 97;
 	  outputChar = vigSquare[userKey[keyCounter]-97][currChar-97];//utilize ony 1 line.
-	  if (makeUpper)
-	    {
+	  if(makeUpper){
 	      outputChar = toupper(outputChar);
 	    }
 	  output = output + outputChar;
 	  //increment char used on key
-
-	  if (keyCounter + 1 >= userKeyCnt)
-	    {
+	  if (keyCounter + 1 >= userKeyCnt){
 	      keyCounter = 0;
-	    }
+	  }
 	  else {
 	    keyCounter++;
 	  }
 	}
-      else
-	{
+      else{
 	  output = output + (char)userInput[i];
-	}
-
+      }
     }
   return output;
 }
@@ -210,47 +184,37 @@ string vigenere::encode(string userInput, string rawUserKey){
 */
 string caesar(string userInput, int userShift) {
   string output = "";
-  for (int i = 0; i < userInput.length(); i++)
-    {
+  for(int i = 0; i < userInput.length(); i++){
       char temp = 'a';
       temp = userInput[i];
       bool makeUpper = false;
-      if (isupper(temp))
-	{
+      if (isupper(temp)){
 	  makeUpper = true;
-	}
-		
+      }		
       //take out case sensitivity
       temp = tolower(temp);
-
       //do the shift if it is a letter
-      if (temp >=96 && temp <=122)
-	{
+      if (temp >=96 && temp <=122){
 	  temp = temp + userShift;
 	  //do corrections
 	  //TODO: Make this into a while
-	  while (temp < 96 || temp > 122)
-	    {
-	      if (temp < 96) //handle undeflows
-		{
-		  //get the difference
-		  int diff = 97 - temp;
-		  temp = 123 - diff;
-		}
+	  while(temp < 96 || temp > 122){
+	    if(temp < 96){ //handle undeflows
+	      //get the difference
+	      int diff = 97 - temp;
+	      temp = 123 - diff;
+	      }
 	      //handle overflows
-	      if (temp > 122)
-		{
+	      if (temp > 122){
 		  int diff = temp - 122;
 		  temp = 96 + diff;
-		}
-				
-	    }
+	      }			
+	   }
 	}
 	
       //commit change
       //handle uppercase
-      if (makeUpper)
-	{
+      if(makeUpper){
 	  temp = toupper(temp);
 	}
       output = output + temp;
@@ -258,35 +222,29 @@ string caesar(string userInput, int userShift) {
   return output;
 }
 
-
-
 /*
   ATBASH CIPHER ENCODER/DECODER FUNCTION
 */
 string atbash(string userInput) {
   string output = "";
-  for (int i = 0; i < userInput.length(); i++)
-    {
+  for(int i = 0; i < userInput.length(); i++){
       char temp = userInput[i];
       bool makeUpper = false;
-      if (isupper(temp))
-	{
+      if(isupper(temp)){
 	  makeUpper = true;
-	}
+      }
       //take out case snsitivity
       temp = tolower(temp);
-      if (temp >=96 || temp <=122) //process supported chars only
-	{
+      if (temp >=96 || temp <=122){ //process supported chars only
 	  int k = 122 - temp;
 	  k = 97 + k;
 			
 	  temp = k;
-	}
+      }
       //process uppercase
-      if (makeUpper)
-	{
+      if (makeUpper){
 	  temp = toupper(temp);
-	}
+      }
       output = output + temp;
     }
   return output;
